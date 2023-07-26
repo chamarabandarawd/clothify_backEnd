75b4d0a1ac996d25a78f14740087f788199b93b3
chamarabandarawd <chamarabandarawd@gmail.com>
2023-07-26 12:16:03 +0530

    setup Sprin

---
+++ src/test/java/icet/clothify/ClothifyApplicationTests.java
@@ -0,0 +1,13 @@
+package icet.clothify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
